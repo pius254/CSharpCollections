58e7698019722c7ad08dfbe439317b856990b190
Pius Muriithi <piusmuriithi1@gmail.com>
2017-12-20 10:37:58 +0300

    Add project files.

---
+++ CSharpCollections/Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCollections
+{
+    public class Arrays
+    {
+        public void CArrays()
+        {
+           //Initialize the array with static elements
+           string[] daysOfWeek =
+           {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            
+            //string tuesday = daysOfWeek[].Length;
+
+            //Loop through the array and display all the items
+            foreach (var item in daysOfWeek)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ CSharpCollections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] daysOfWeek =
+            //{
+            //    "Monday",
+            //    "Tuesday",
+            //    "Wednesday",
+            //    "Thursday",
+            //    "Friday",
+            //    "Saturday",
+            //    "Sunday"
+            //};
+
+            ////string tuesday = daysOfWeek[].Length;
+            //foreach (var item in daysOfWeek)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
+            //Console.WriteLine();
+            Arrays showArrays = new Arrays();
+            showArrays.CArrays();
+        }
+    }
+}
