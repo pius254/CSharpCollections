d7729e3a4478c635e3485e716174a25428b2dbd4
Pius Muriithi <piusmuriithi1@gmail.com>
2017-12-21 12:13:01 +0300

    Reverse the array

--- CSharpCollections/Arrays.cs
+++ CSharpCollections/Arrays.cs
@@ -20,9 +20,19 @@ namespace CSharpCollections
                 "Sunday"
             };
             
-            //string tuesday = daysOfWeek[].Length;
+            //string length = daysOfWeek[].Length;
 
             //Loop through the array and display all the items
+            foreach (var item in daysOfWeek)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            //Reverse the items in the array
+            Array.Reverse(daysOfWeek);
+
+            Console.WriteLine();
+
             foreach (var item in daysOfWeek)
             {
                 Console.WriteLine(item.ToString());
